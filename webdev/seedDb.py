725e748a7d5224785ac71e73dfd639e6d009b44d
kevinshi97 <kevinsshi@hotmail.com>
2018-06-18 16:17:19 -0400

    styling for graphs

--- webdev/seedDb.py
+++ webdev/seedDb.py
@@ -36,7 +36,7 @@ for i in range(1, 501):
 	val = float(i)/float(1000) * 100
 	val2 = float(1000- i)/float(1000) * 100
 	val3 = float(i * 2)/float(1000) * 100
-	val4 = float(100 - i * 2)/float(1000) * 100
+	val4 = float(1000 - i * 2)/float(1000) * 100
 	datetime = 'NOW() + INTERVAL %d HOUR'%(i)
 	c.execute('INSERT INTO raw_data VALUES(%d, %f, YEAR(%s), MONTH(%s), DAY(%s), HOUR(%s), %s, %s, 1, 1, 1);'%(i, val, datetime, datetime, datetime, datetime, datetime, datetime))
 	c.execute('INSERT INTO raw_data VALUES(%d, %f, YEAR(%s), MONTH(%s), DAY(%s), HOUR(%s), %s, %s, 2, 1, 1);'%(500 + i, val2, datetime, datetime, datetime, datetime, datetime, datetime))
--- webdev/views/assets/js/graph.ts
+++ webdev/views/assets/js/graph.ts
@@ -1,6 +1,7 @@
 import Chart = require('chart.js');
 import moment = require('moment');
 import * as $ from 'jquery';
+import 'bootstrap';
 
 let timeFormat: string = 'MM/DD/YYYY HH:mm';
 let curr_datetime: moment.Moment = moment();
@@ -50,20 +51,22 @@ function makeChart(ctx: CanvasRenderingContext2D, title: string, xLabel: string,
                         tooltipFormat: 'll HH:mm'
                     },
                     scaleLabel: {
-                        display: true,
-                        labelString: xLabel,
-                        // fontSize: 8
+                        display: true
                     }
                 }],
                 yAxes: [{
                     scaleLabel: {
                         display: true,
-                        labelString: yLabel
+                        labelString: yLabel,
+                    },
+                    ticks: {
+                        min: 0,
+                        max: 100
                     }
                 }]
             },
             legend: {
-                position: 'left'
+                position: 'right'
             }
         }
     });
@@ -71,7 +74,7 @@ function makeChart(ctx: CanvasRenderingContext2D, title: string, xLabel: string,
 
 function makeDataset(label: string): Chart.ChartDataSets{
     return {
-        label: 'Methane LEL',
+        label: label,
         backgroundColor: 'rgb(255,0,0,1)',
         pointHoverBackgroundColor: 'rgb(255,0,0,1)',
         borderColor: 'rgb(255,0,0,1)',
@@ -84,7 +87,7 @@ let htmlCanvas: any = document.getElementById("methane_chart");
 let methane_chart: Chart = makeChart(htmlCanvas.getContext('2d'), 'Methane', 'Time', '% of LEL');
 
 htmlCanvas = document.getElementById("ammonia_chart");
-let ammonia_chart: Chart = makeChart(htmlCanvas.getContext('2d'), 'Amonia', 'Time', 'Concentration');
+let ammonia_chart: Chart = makeChart(htmlCanvas.getContext('2d'), 'Ammonia', 'Time', 'Concentration');
 
 htmlCanvas = document.getElementById("hydrogen_sulfide_chart");
 let hydrogen_sulfide_chart: Chart = makeChart(htmlCanvas.getContext('2d'), 'Hydrogen Sulfide', 'Time', 'Concentration');
@@ -99,7 +102,7 @@ htmlCanvas = document.getElementById("humidity_chart");
 let humidity_chart: Chart = makeChart(htmlCanvas.getContext('2d'), 'Relative Humidity', 'Time', 'Relative Humidity');
 
 let methane_min: Chart.ChartDataSets = makeDataset('Methane LEL');
-let ammonia_min: Chart.ChartDataSets = makeDataset('Amonia Lower Limit');
+let ammonia_min: Chart.ChartDataSets = makeDataset('Ammonia Lower Limit');
 let hydrogen_sulfide_min: Chart.ChartDataSets = makeDataset('Hydrogen Sulfide Lower Limit');
 let carbon_dioxide_min: Chart.ChartDataSets = makeDataset('Carbon Dioxide LEL');
 let temp_max: Chart.ChartDataSets = makeDataset('Temperature Upper Limit');
@@ -114,16 +117,6 @@ function randomColor(): string {
     let to_return: string = 'rgb('+ r +','+ g +','+ b +''+ a +')'
     return to_return;
 }
-
-// function parseData(data: Array<{t: string, y: number, sensor_name: string}>): Array<{t: Date, y: number}>{
-//     let parsed_data: Array<{t: Date, y: number}> = [];
-    
-//     for (let i = 0; i < data.length; i++) {
-//         let new_data: {t: Date, y: number} = {t: new Date(data[i].t), y: data[i].y};
-//         parsed_data.push(new_data);
-//     }
-//     return parsed_data;
-// }
 
 function setLimitBounds(start_datetime: Date, finsih_datetime: Date): void {
     methane_min.data = [{t: start_datetime, y: 30}, {t: finsih_datetime, y: 30}];
@@ -219,9 +212,6 @@ function processResponse(response: any) {
                 series_types[data[i].id] = data[i].type;
             }
         }
-        console.log(series_data);
-        console.log(series_names);
-        console.log(series_types);
 
         let labels = createLabels();
 
@@ -263,6 +253,35 @@ function processResponse(response: any) {
                 addDataset(humidity_chart, labels, dataset);
             }
         }
+        $('.nav-link').show();
+        $('.nav-link').parent().show();
+
+        let charts: Array<Chart> = [methane_chart, ammonia_chart, carbon_dioxide_chart, hydrogen_sulfide_chart, temp_chart, humidity_chart];
+        let tabs: Array<JQuery<HTMLElement>> = [$('#methane-tab'), $('#ammonia-tab'), $('#carbon-dioxide-tab'), $('#hydrogen-sulfide-tab'), $('#temp-tab'), $('#humidity-tab')];
+        let panes: Array<JQuery<HTMLElement>> = [$('#methane'), $('#ammonia'), $('#carbon-dioxide'), $('#hydrogen-sulfide'), $('#temp'), $('#humidity')];
+        let showing: any = [];
+        let showing2: any = []
+
+        for(let i = 0; i < charts.length; i ++) {
+            tabs[i].removeClass('active');
+            panes[i].removeClass('active');
+            panes[i].removeClass('show');
+            charts[i].update();
+            if(charts[i].data.datasets.length == 1 || (i == 4 && charts[i].data.datasets.length == 2)){
+                tabs[i].hide();
+                tabs[i].parent().hide();
+            }
+            else {
+                showing.push(tabs[i]);
+                showing2.push(panes[i]);
+            }
+        }
+        if(showing.length > 0) {
+            showing[0].addClass('active');
+            showing2[0].addClass('active');
+            showing2[0].addClass('show');
+            // showing[0].tab('show') // Select first tab
+        }
     }
     else {
         let labels = createLabels();
@@ -283,147 +302,6 @@ function processResponse(response: any) {
         addDataset(humidity_chart, labels, humidity_min); 
     }
 }
-
-$(document).ready(function() {    
-    $.get('/graph/basestations', function(response) {
-        let data = response.data;
-        if (data.length > 0) {
-            for(let i = 0 ; i < data.length; i++) {
-                basestations[data[i].id] = new Basestation(data[i].id, data[i].name, data[i].description);
-                
-            }
-        }
-    }).then(function() {
-        $.get('/graph/groups', function(response) {
-            // processResponse(response);
-            let data = response.data;
-            if(data.length > 0) {
-                for(let i = 0; i < data.length; i++) {
-                    if(basestations[data[i].basestation_id]){
-                        basestations[data[i].basestation_id].add_group(new Group(data[i].id, data[i].name, data[i].description));
-                    }
-                }
-            }
-        });
-    }).then(function() {
-        for(let id in basestations){
-            $('#basestation_select').append($('<option>', {
-                value: basestations[id].id,
-                text: '(' + basestations[id].id + ')' + ' ' + basestations[id].name,
-                title: basestations[id].description
-            }));
-        }
-    }).then(function() {
-       request_data();
-    });
-});
-
-$('#time_unit_select').change(function() {
-    let time_unit: string = $('#time_unit_select').val().toString();
-    if(time_unit === 'hourly'){
-        start_datetime = moment(curr_datetime).startOf('hour');
-        end_datetime = moment(curr_datetime).endOf('hour');
-    }
-    else if(time_unit === 'daily'){
-        start_datetime = moment(curr_datetime).startOf('day');
-        end_datetime = moment(curr_datetime).endOf('day');
-    }
-    else if(time_unit === 'monthly'){
-        start_datetime = moment(curr_datetime).startOf('month');
-        end_datetime = moment(curr_datetime).endOf('month');
-    }
-    else if(time_unit === 'yearly'){
-        start_datetime = moment(curr_datetime).startOf('year');
-        end_datetime = moment(curr_datetime).endOf('year');
-    }
-    createLabels();
-    setLimitBounds(start_datetime.toDate(), end_datetime.toDate());
-    request_data();
-});
-
-$('#basestation_select').change(function() {
-    let id = Number($('#basestation_select').val());
-    if(basestations[id]) {
-        $('#group_select').html('');
-        $('#group_select').append($('<option>', {
-                value: null,
-                text: 'None'
-            }));
-
-        let groups = basestations[id].groups;
-        for(let i = 0; i < groups.length; i++){
-            $('#group_select').append($('<option>', {
-                value: groups[i].id,
-                text: '(' + groups[i].id + ')' + ' ' + groups[i].name,
-                title: groups[i].description
-            }));    
-        }
-    }
-    createLabels();
-    setLimitBounds(start_datetime.toDate(), end_datetime.toDate());
-    request_data();
-});
-
-$('#group_select').change(function() {
-    createLabels();
-    setLimitBounds(start_datetime.toDate(), end_datetime.toDate());
-    request_data();
-});
-
-$('#previous_btn').click(function() {
-    let time_unit: string = $('#time_unit_select').val().toString();
-    if(time_unit === 'hourly'){
-        curr_datetime.subtract(1, 'hour');
-        start_datetime = moment(curr_datetime).startOf('hour');
-        end_datetime = moment(curr_datetime).endOf('hour');
-    }
-    else if(time_unit === 'daily'){
-        curr_datetime.subtract(1, 'day');
-        start_datetime = moment(curr_datetime).startOf('day');
-        end_datetime = moment(curr_datetime).endOf('day');
-    }
-    else if(time_unit === 'monthly'){
-        curr_datetime.subtract(1, 'month');
-        start_datetime = moment(curr_datetime).startOf('month');
-        end_datetime = moment(curr_datetime).endOf('month');
-    }
-    else if(time_unit === 'yearly'){
-        curr_datetime.subtract(1, 'year');
-        start_datetime = moment(curr_datetime).startOf('year');
-        end_datetime = moment(curr_datetime).endOf('year');
-    }
-    createLabels();
-    setLimitBounds(start_datetime.toDate(), end_datetime.toDate());
-    request_data();
-})
-
-$('#next_btn').click(function() {
-    let time_unit: string = $('#time_unit_select').val().toString();
-    if(time_unit === 'hourly'){
-        curr_datetime.add(1, 'hour');
-        start_datetime = moment(curr_datetime).startOf('hour');
-        end_datetime = moment(curr_datetime).endOf('hour');
-    }
-    else if(time_unit === 'daily'){
-        curr_datetime.add(1, 'day');
-        start_datetime = moment(curr_datetime).startOf('day');
-        end_datetime = moment(curr_datetime).endOf('day');
-    }
-    else if(time_unit === 'monthly'){
-        curr_datetime.add(1, 'month');
-        start_datetime = moment(curr_datetime).startOf('month');
-        end_datetime = moment(curr_datetime).endOf('month');
-    }
-    else if(time_unit === 'yearly'){
-        curr_datetime.add(1, 'year');
-        start_datetime = moment(curr_datetime).startOf('year');
-        end_datetime = moment(curr_datetime).endOf('year');
-    }
-    createLabels();
-    setLimitBounds(start_datetime.toDate(), end_datetime.toDate());
-    request_data();
-})
-
 function request_data(){
     let time_unit: string = $('#time_unit_select').val().toString();
     let start_date: string = start_datetime.toDate().toString();
@@ -440,7 +318,188 @@ function request_data(){
         {time_unit: time_unit, start_date: start_date, end_date: end_date, b_id: b_id, g_id: g_id}, 
         function(response) {
             processResponse(response);
-            $('#timetitle').html('' + start_datetime.format(timeFormat) + ' - ' + end_datetime.format(timeFormat));
+            if(time_unit === 'hourly'){
+                let temp: moment.Moment = moment(end_datetime).add(1, 'hour');
+                $('#timetitle').html('' + start_datetime.format('MMMM Do YYYY, h a') + ' - ' + temp.format('h a'));
+            }
+            else if(time_unit === 'daily'){
+                $('#timetitle').html('' + start_datetime.format('MMMM Do YYYY'));
+            }
+            else if(time_unit === 'monthly'){
+                $('#timetitle').html('' + start_datetime.format('MMMM YYYY'));
+            }
+            else if(time_unit === 'yearly'){
+                $('#timetitle').html('' + start_datetime.format('YYYY'));
+            }
+            
         }
     );
 }
+
+$(document).ready(function() {
+    let first_id: number;    
+    $.get('/graph/basestations', function(response) {
+        let data = response.data;
+        if (data.length > 0) {
+            for(let i = 0 ; i < data.length; i++) {
+                basestations[data[i].id] = new Basestation(data[i].id, data[i].name, data[i].description);
+                if(i == 0){
+                    first_id = data[i].id;
+                }
+            }
+        }
+    }).then(function() {
+        $.get('/graph/groups', function(response) {
+            // processResponse(response);
+            let data = response.data;
+            if(data.length > 0) {
+                for(let i = 0; i < data.length; i++) {
+                    if(basestations[data[i].basestation_id]){
+                        basestations[data[i].basestation_id].add_group(new Group(data[i].id, data[i].name, data[i].description));
+
+                        if(data[i].basestation_id == first_id){
+                            $('#group_select').append($('<option>', {
+                                value: data[i].id,
+                                text: '(' + data[i].id + ')' + ' ' + data[i].name,
+                                title: data[i].description
+                            })); 
+                        }
+                    }
+                }
+            }
+        });
+    }).then(function() {
+        for(let id in basestations){
+            $('#basestation_select').append($('<option>', {
+                value: basestations[id].id,
+                text: '(' + basestations[id].id + ')' + ' ' + basestations[id].name,
+                title: basestations[id].description
+            }));
+        }        
+    }).then(function() {
+       request_data();
+    }).then(function() {
+        $('#time_unit_select').change(function() {
+            let time_unit: string = $('#time_unit_select').val().toString();
+            if(time_unit === 'hourly'){
+                start_datetime = moment(curr_datetime).startOf('hour');
+                end_datetime = moment(curr_datetime).endOf('hour');
+
+                $("#previous_btn").prop('value', 'Prev. Hour');
+                $("#next_btn").prop('value', 'Next  Hour');
+            }
+            else if(time_unit === 'daily'){
+                start_datetime = moment(curr_datetime).startOf('day');
+                end_datetime = moment(curr_datetime).endOf('day');
+
+                $("#previous_btn").prop('value', 'Prev. Day');
+                $("#next_btn").prop('value', 'Next  Day');
+            }
+            else if(time_unit === 'monthly'){
+                start_datetime = moment(curr_datetime).startOf('month');
+                end_datetime = moment(curr_datetime).endOf('month');
+
+                $("#previous_btn").prop('value', 'Prev. Month');
+                $("#next_btn").prop('value', 'Next  Month');
+            }
+            else if(time_unit === 'yearly'){
+                start_datetime = moment(curr_datetime).startOf('year');
+                end_datetime = moment(curr_datetime).endOf('year');
+
+                $("#previous_btn").prop('value', 'Prev. Year');
+                $("#next_btn").prop('value', 'Next  Year');
+            }
+            createLabels();
+            setLimitBounds(start_datetime.toDate(), end_datetime.toDate());
+            request_data();
+        });
+
+        $('#basestation_select').change(function() {
+            let id = Number($('#basestation_select').val());
+            if(basestations[id]) {
+                $('#group_select').html('');
+                $('#group_select').append($('<option>', {
+                        value: null,
+                        text: 'None'
+                    }));
+
+                let groups = basestations[id].groups;
+
+                console.log(id);
+                console.log(groups);
+
+                for(let i = 0; i < groups.length; i++){
+                    $('#group_select').append($('<option>', {
+                        value: groups[i].id,
+                        text: '(' + groups[i].id + ')' + ' ' + groups[i].name,
+                        title: groups[i].description
+                    }));    
+                }
+            }
+            createLabels();
+            setLimitBounds(start_datetime.toDate(), end_datetime.toDate());
+            request_data();
+        });
+
+        $('#group_select').change(function() {
+            createLabels();
+            setLimitBounds(start_datetime.toDate(), end_datetime.toDate());
+            request_data();
+        });
+
+        $('#previous_btn').click(function() {
+            let time_unit: string = $('#time_unit_select').val().toString();
+            if(time_unit === 'hourly'){
+                curr_datetime.subtract(1, 'hour');
+                start_datetime = moment(curr_datetime).startOf('hour');
+                end_datetime = moment(curr_datetime).endOf('hour');
+            }
+            else if(time_unit === 'daily'){
+                curr_datetime.subtract(1, 'day');
+                start_datetime = moment(curr_datetime).startOf('day');
+                end_datetime = moment(curr_datetime).endOf('day');
+            }
+            else if(time_unit === 'monthly'){
+                curr_datetime.subtract(1, 'month');
+                start_datetime = moment(curr_datetime).startOf('month');
+                end_datetime = moment(curr_datetime).endOf('month');
+            }
+            else if(time_unit === 'yearly'){
+                curr_datetime.subtract(1, 'year');
+                start_datetime = moment(curr_datetime).startOf('year');
+                end_datetime = moment(curr_datetime).endOf('year');
+            }
+            createLabels();
+            setLimitBounds(start_datetime.toDate(), end_datetime.toDate());
+            request_data();
+        })
+
+        $('#next_btn').click(function() {
+            let time_unit: string = $('#time_unit_select').val().toString();
+            if(time_unit === 'hourly'){
+                curr_datetime.add(1, 'hour');
+                start_datetime = moment(curr_datetime).startOf('hour');
+                end_datetime = moment(curr_datetime).endOf('hour');
+            }
+            else if(time_unit === 'daily'){
+                curr_datetime.add(1, 'day');
+                start_datetime = moment(curr_datetime).startOf('day');
+                end_datetime = moment(curr_datetime).endOf('day');
+            }
+            else if(time_unit === 'monthly'){
+                curr_datetime.add(1, 'month');
+                start_datetime = moment(curr_datetime).startOf('month');
+                end_datetime = moment(curr_datetime).endOf('month');
+            }
+            else if(time_unit === 'yearly'){
+                curr_datetime.add(1, 'year');
+                start_datetime = moment(curr_datetime).startOf('year');
+                end_datetime = moment(curr_datetime).endOf('year');
+            }
+            createLabels();
+            setLimitBounds(start_datetime.toDate(), end_datetime.toDate());
+            request_data();
+        })
+    })
+});
+
