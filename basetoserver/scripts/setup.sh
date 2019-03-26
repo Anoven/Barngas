afbf5f85c129a12f79c1344b810e161a1abc4104
root <vasav.shah97@gmail.com>
2019-03-26 06:02:09 +0000

    added setup scripts to automatically setup chrom browser startup, and started the firmware update script

---
+++ basetoserver/scripts/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+CHROMESETUPFILE="/home/pi/.config/lxsession/LXDE-pi/autostart"
+CHROMESETUPCOMMAND="@/usr/bin/chromium-browser --kiosk --disable-restore-session-state http://138.197.156.151:3000/login"
+
+#sets up the autostart to chrome harvest measurement
+if [ -f $CHROMESETUPFILE ]
+then
+	if [ $(grep -c "chromium" $CHROMESETUPFILE) -eq 0 ]
+	then
+		echo "chromium" >> $CHROMESETUPFILE
+	else
+		echo "chromium setup already!"
+	fi
+else
+	echo "setup file does not exsist!"
+fi
