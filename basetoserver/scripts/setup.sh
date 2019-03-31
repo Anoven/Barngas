1f1ca7fb442370b92eebe1fdf22d31ccd6412d40
Vasav Shah <vasav.shah97@gmail.com>
2019-03-31 05:27:59 +0000

    added some autostart stuff, refined start script to only start after internet connection

--- basetoserver/scripts/setup.sh
+++ basetoserver/scripts/setup.sh
@@ -1,23 +1,46 @@
 #!/bin/sh
+SETUP_MQTT=0
+SETUP_CHROMIUM=0
+SETUP_AUTOSTART=1
+
 CHROMESETUPFILE="/home/pi/.config/lxsession/LXDE-pi/autostart"
 CHROMESETUPCOMMAND="@/usr/bin/chromium-browser --kiosk --disable-restore-session-state http://138.197.156.151:3000/login"
 
-#sets up the autostart to chrome harvest measurement
-if [ -f $CHROMESETUPFILE ]
+HARVESTAUTOSTARTFILE="/etc/rc.local"
+
+if [ $SETUP_CHROMIUM -eq 1 ]
 then
-	if [ $(grep -c "chromium" $CHROMESETUPFILE) -eq 0 ]
+	#sets up the autostart to chrome harvestmeasurement
+	if [ -f $CHROMESETUPFILE ]
 	then
-		echo $CHROMESETUPCOMMAND >> $CHROMESETUPFILE
+		if [ $(grep -c "chromium" $CHROMESETUPFILE) -eq 0 ]
+		then
+			echo $CHROMESETUPCOMMAND >> $CHROMESETUPFILE
+		else
+			echo "chromium setup already!"
+		fi
 	else
-		echo "chromium setup already!"
+		echo "setup file does not exsist!"
 	fi
-else
-	echo "setup file does not exsist!"
 fi
 
-#set up mqtt libraries
-apt-get update
-apt-get install mosquitto
-apt-get install mosquitto-clients
-python3 -m pip install paho-mqtt
-service mosquitto start
+if [ $SETUP_MQTT -eq 1 ]
+then
+	#set up mqtt libraries
+	apt-get update
+	apt-get install mosquitto
+	apt-get install mosquitto-clients
+	python3 -m pip install paho-mqtt
+	service mosquitto start
+fi
+
+if [ $SETUP_AUTOSTART -eq 1 ]
+then
+	#set up autostart code
+	if [ $(grep -c "start.sh" $HARVESTAUTOSTARTFILE) -eq 0 ]
+	then
+		echo "/home/pi/Barngas/basetoserver/scripts/start.sh" >> $HARVESTAUTOSTARTFILE
+	else
+		echo "autostart setup already!"
+	fi
+fi
--- basetoserver/scripts/start.sh
+++ basetoserver/scripts/start.sh
@@ -1,5 +1,26 @@
-pon rogers
-BASESTATIONFOLDER=/home/pi/Barngas/basetoserver/
-cd $BASESTATIONFOLDER
-python3 main.py
+#!/bin/sh
+pon rogers&
+sleep 5
+rc=1
+count=100                            # Maximum number to try.
+while [ $count -ge 0 ]
+do
+    ping -c 1 138.197.156.151                      # Try once.
+    rc=$?
+    echo "rc $rc count $count">>/home/pi/test.txt
+    if [ $rc -eq 0 ]
+    then
+	BASESTATIONFOLDER=/home/pi/Barngas/basetoserver/
+	cd $BASESTATIONFOLDER
+	python3 main.py&
+	echo "started!" >> /home/pi/test.txt
+	exit 0
+   fi
+   count=$(($count-1))
+   sleep 2
+done
+echo $rc>>/home/pi/test.txt
+echo "couldn't connect to server! ">>/home/pi/test.txt
+
+exit 1
 
