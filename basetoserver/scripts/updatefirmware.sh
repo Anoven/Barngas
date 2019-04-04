725ac5aed0d7c15aaf837a21152fecd85eef4d8e
Vasav Shah <vasav.shah97@gmail.com>
2019-04-04 04:51:33 +0000

    shutdown ssh tunnel when updating firmware

--- basetoserver/scripts/updatefirmware.sh
+++ basetoserver/scripts/updatefirmware.sh
@@ -2,5 +2,6 @@
 
 echo "updating firmware!"
 killall -9 python3
+killall -9 ssh
 sshpass -p "serverpassword"scp -r root@138.197.156.151:/root/Base_release/Barngas /home/pi/
 /home/pi/Barngas/basetoserver/scripts/start.sh
